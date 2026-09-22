faa1ead069f41176cd29a3717e3b187ac76cef79
凉宫春日 <Admin@MacBook-Air.local>
2026-09-23 00:28:22 +0800

    Complete lab01

--- src/lab_68662FBB.c
+++ src/lab_68662FBB.c
@@ -2,7 +2,13 @@
 
 void printscreen()
 {
-    // Please write your code here
+    printf("####\n");
+    printf("# #\n");
+    printf("# #\n");
+    printf("######\n");
+    printf("# #\n");
+    printf("# #\n");
+    printf("# #\n");
 }
 
 // DO NOT change the code below
