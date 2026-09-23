cbc86a21b1a605ac2e691a1c226db5c45dd6a08c
凉宫春日 <Admin@MacBook-Air.local>
2026-09-23 11:30:39 +0800

    Complete lab01

--- src/lab_68662FBB.c
+++ src/lab_68662FBB.c
@@ -2,13 +2,13 @@
 
 void printscreen()
 {
-    printf("####\n");
-    printf("# #\n");
-    printf("# #\n");
+    printf(" ####\n");
+    printf("#    #\n");
+    printf("#    #\n");
     printf("######\n");
-    printf("# #\n");
-    printf("# #\n");
-    printf("# #\n");
+    printf("#    #\n");
+    printf("#    #\n");
+    printf("#    #\n");
 }
 
 // DO NOT change the code below
